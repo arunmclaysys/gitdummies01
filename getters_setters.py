5267bab15440738f71ad271956bc0c9708f5c9f1
Arun M <arun.m.claysys@gmail.com>
2023-08-08 21:36:55 +0530

    Added getters(property) in code

---
+++ getters_setters.py
@@ -0,0 +1,28 @@
+class BankAccount:
+    def __init__(self):
+        self._account_balance = 0  #Private Variable
+
+    @property   #this means getter
+    def account_balance(self):
+        return self._account_balance
+
+    def deposit(self, amount):
+        self._account_balance += amount
+
+    def withdraw(self, amount):
+        self._account_balance -= amount
+
+    '''def __str__(self):
+        return f"Balance: {self.account_balance}"
+'''
+
+def main():
+    bank_account = BankAccount()
+    print(bank_account.account_balance)
+    bank_account.deposit(110)
+    bank_account.withdraw(51)
+    print(bank_account.account_balance)
+
+
+if __name__ == "__main__":
+    main()
